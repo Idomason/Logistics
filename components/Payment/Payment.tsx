12e94a32c8ceb013dc4db4285afc811223f24d9e
Idoma Ngbede <idomakevin@gmail.com>
2024-01-02 00:18:10 +0100

    made some changes in the payment component

--- components/Payment/Payment.tsx
+++ components/Payment/Payment.tsx
@@ -63,7 +63,7 @@ export default function Payment() {
         />
       </div>
       <h1 className='text-xl md:text-4xl md:self-start md:mb-4 md:w-1/2 md:leading-normal font-bold py-4 text-black'>
-        Everything you need, less of what you don't.
+        Everything you need, less of what you do not.
       </h1>
       <div className='w-full hidden md:flex justify-between'>
         <div className='text-gray-500 hidden md:block'>
